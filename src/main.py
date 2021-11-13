abcaefa735af673ef0e40c405a5640db28e2289c
Nayeon <dev.nayeon.shin@gmail.com>
2021-11-14 03:33:00 +0900

    feat(main.py): Add index() and run it

--- src/main.py
+++ src/main.py
@@ -1,6 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask("Insta Word Cloud")
+
+
+@app.route("/")
+def index():
+    return "Hello, World!"
+
 
 if __name__ == "__main__":
     app.run()
