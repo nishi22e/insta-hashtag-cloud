9ff2245474aa120edb58722d4cfb60306e8f023e
Nayeon <dev.nayeon.shin@gmail.com>
2021-11-14 05:19:22 +0900

    feat(insta_mining.py): Don't let browser quit immediately

--- insta_mining.py
+++ insta_mining.py
@@ -8,10 +8,14 @@ driver = webdriver.Chrome(ChromeDriverManager().install())
 hashtag = "dog"  # Temporary; need to get user input
 driver.get(f"https://www.instagram.com/explore/tags/{hashtag}/")
 
+
 search_bar = driver.find_element_by_class_name("XTCLo")
 
-search_bar.send_keys("hello")
+search_bar.send_keys("#hello")
 print(search_bar)
+
+while True:
+    pass
 
 # time.sleep(3)
 # driver.quit()
