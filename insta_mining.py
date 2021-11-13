8d789e7276f6dcb9e78d1c5458bbd12e72b0fc8d
Nayeon <dev.nayeon.shin@gmail.com>
2021-11-14 07:46:25 +0900

    fix(insta_mining.py): Get back drivermanager code and reformat

--- insta_mining.py
+++ insta_mining.py
@@ -5,19 +5,20 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium import webdriver
 
-####################### Navigate to Insta#####################
+from webdriver_manager.chrome import ChromeDriverManager
 
-driver_path = "/Users/emilynishikimoto/Desktop/insta-word-cloud/chromedriver"
-driver = webdriver.Chrome(driver_path)
+driver = webdriver.Chrome(ChromeDriverManager().install())
 
 hashtag = "#dog"  # Temporary; need to get user input
 driver.get(f"https://www.instagram.com/")
 
-########################log in##################################
+username = WebDriverWait(driver, 10).until(
+    EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='username']"))
+)
 
-username = WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='username']")))
-
-password = WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='password']")))
+password = WebDriverWait(driver, 10).until(
+    EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='password']"))
+)
 
 username.clear()
 my_username = input("Input your username: \n")
@@ -27,13 +28,29 @@ password.clear()
 my_password = input("Input your password: \n")
 password.send_keys(my_password)
 
-login_button = WebDriverWait(driver,2).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit']"))).click()
+login_button = (
+    WebDriverWait(driver, 2)
+    .until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit']")))
+    .click()
+)
 
 ################### Manage Alerts###########################
 
-not_now = WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH, '//button[contains(text(), "Not Now")]'))).click()
+not_now = (
+    WebDriverWait(driver, 10)
+    .until(
+        EC.element_to_be_clickable((By.XPATH, '//button[contains(text(), "Not Now")]'))
+    )
+    .click()
+)
 
-not_now2 = WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH, '//button[contains(text(), "Not Now")]'))).click()
+not_now2 = (
+    WebDriverWait(driver, 10)
+    .until(
+        EC.element_to_be_clickable((By.XPATH, '//button[contains(text(), "Not Now")]'))
+    )
+    .click()
+)
 
 ################# navigate to search bar ###############
 search_bar = driver.find_element_by_class_name("XTCLo")
@@ -41,15 +58,14 @@ search_bar.send_keys(hashtag)
 
 ################### Find 10 most relevant hashtags and their num of posts ########
 
-#key = hashtag, value = num posts
+# key = hashtag, value = num posts
 related_hashtags = {}
 
 # for i in range(10):
 
 
-
-
-
+while True:
+    pass
 # print(search_bar)
 
 # time.sleep(3)
