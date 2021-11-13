8c8cb4be8098f1b2a59ee4c0fb426a66e9bb3adc
Nayeon <dev.nayeon.shin@gmail.com>
2021-11-14 07:47:18 +0900

    style(insta_mining.py): isort

--- insta_mining.py
+++ insta_mining.py
@@ -1,10 +1,10 @@
 import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
-from selenium import webdriver
-
 from webdriver_manager.chrome import ChromeDriverManager
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
