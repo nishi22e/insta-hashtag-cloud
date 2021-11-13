67cb6540c652911ad7992dbd3f781eb2d899af77
Nayeon <dev.nayeon.shin@gmail.com>
2021-11-14 02:55:39 +0900

    feat(main.py): Add dog code

--- src/main.py
+++ src/main.py
@@ -1,4 +1,12 @@
+import time
+
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
+
+hashtag = "dog"  # Temporary; need to get user input
+driver.get(f"https://www.instagram.com/explore/tags/{hashtag}/")
+
+time.sleep(3)
+driver.quit()
