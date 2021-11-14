9e5f8a6399cd1f98f4a32b22270322e200e1f9a5
Nayeon <dev.nayeon.shin@gmail.com>
2021-11-14 09:21:21 +0900

    feat(insta_mining.py): dropdown_menu

--- insta_mining.py
+++ insta_mining.py
@@ -1,5 +1,7 @@
 import time
 
+# from bs4 import BeautifulSoup
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -8,24 +10,22 @@ from selenium.webdriver.support.wait import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
-
-hashtag = "#dog"  # Temporary; need to get user input
 driver.get(f"https://www.instagram.com/")
 
+# Log in
 username = WebDriverWait(driver, 10).until(
     EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='username']"))
 )
-
 password = WebDriverWait(driver, 10).until(
     EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='password']"))
 )
 
 username.clear()
-my_username = input("Input your username: \n")
+my_username = input("Input your username: ")
 username.send_keys(my_username)
 
 password.clear()
-my_password = input("Input your password: \n")
+my_password = input("Input your password: ")
 password.send_keys(my_password)
 
 login_button = (
@@ -34,8 +34,7 @@ login_button = (
     .click()
 )
 
-# Manage Alerts
-
+# Manage alerts
 not_now = (
     WebDriverWait(driver, 10)
     .until(
@@ -43,7 +42,6 @@ not_now = (
     )
     .click()
 )
-
 not_now2 = (
     WebDriverWait(driver, 10)
     .until(
@@ -52,15 +50,22 @@ not_now2 = (
     .click()
 )
 
+# Navigate to search bar
 search_bar = driver.find_element_by_class_name("XTCLo")
+hashtag = input("Enter a hashtag: ")
 search_bar.send_keys(hashtag)
 
+dropdown_menu = WebDriverWait(driver, 5).until(
+    EC.presence_of_element_located((By.CLASS_NAME, "fuqBx"))
+)
+divs = dropdown_menu.find_elements_by_tag_name("div")
+print(divs)
 
-# key = hashtag, value = num posts
+
+
 related_hashtags = {}
 
 # for i in range(10):
-
 
 while True:
     pass
